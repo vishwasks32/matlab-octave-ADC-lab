79f36cb7f0b28f49b58198ee1c18b0af6f2a2861
vishwasks32 <vishwasks32@users.noreply.github.com>
2016-12-01 07:36:12 +0530

    ADC lab codes

---
+++ ask_vi.m
@@ -0,0 +1,68 @@
+% Program to simulate ASK
+% Author : Vishwas K Singh
+% Date 11 - 11 - 2016
+
+clear all;
+close all;
+clc;
+
+% Input Sequence and size
+x = [1 1 0 0 1 0 1 0 1];
+nx = size(x,2);
+
+% Generate the carrier frequency with say 5kHz
+f = 5;
+tc = 1:0.001:9;
+fc = sin(2 * pi * f * tc);
+
+p = 1
+while p < nx+1
+	if x[p] == 1
+		ask = sin(2*pi*f*t)
+	else
+		ask = 0
+	endif
+	p = p + 1;
+endwhile
+
+% Perform ASK
+i = 1;
+while i < nx+1
+	t = i:0.001:i+1;
+	if x[i] == 1
+		ask = sin(2*pi*f*t)
+	else
+		ask = 0
+	endif
+	i = i + 1;
+endwhile
+
+% Plot the sequence				
+subplot(3,1,1);
+stem(x);
+axis([1 9 -1 1]);
+title('Input Sequence');
+xlabel('Time');
+ylabel('Amplitude');
+grid on;
+hold on;
+
+% Plot the carrier
+subplot(3,1,2);
+plot(tc,fc);
+axis([1 9 -1 1]);
+title('Carrier Frequency');
+xlabel('Time');
+ylabel('Amplitude');
+grid on;
+hold on;
+
+% Plot the ASK Signal
+subplot(3,1,3);
+plot(t,ask);
+axis([1 9 -1 1]);
+title('ASK');
+xlabel('Time');
+ylabel('Amplitude');
+grid on;
+hold on;
